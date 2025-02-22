38557b3f89c53521f91fac178fb4889d5a29cec8
marselhanane <s12220672@stu.najah.edu>
2025-02-22 11:57:50 +0200

    last edit

--- models/Audit.model.js
+++ models/Audit.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const auditSchema = mongoose.Schema({
     action: { type: String, required: true },
     status:{type:Number, required: true},
-    errors:{type:Array, required:true},
+    errorMessages:{type:Array, required:true},
     auditBy: { type: String, required: true },
     auditOn: { type: Date, default: Date.now },
     data: { type: Object }
